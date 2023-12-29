edcfc4693d13e8a2b399174b315a7868d0d36be2
peter-seweiha <peter.seweiha@gmail.com>
2023-12-30 09:37:26 +1100

    capture images using capture_array

--- Code/Raspberry pi/camera_preview.py
+++ Code/Raspberry pi/camera_preview.py
@@ -1,11 +1,13 @@
 from picamera2 import Picamera2, Preview
 import time
+import cv2
+import pandas as pd
 
 
 # Create a PiCamera object
 picam2 = Picamera2()
 #  Create a new object, camera_config and use it to set the still image resolution (main) to 1920 x 1080. and a lowres image with a size of 640 x 480. This lowres image is used as the preview image when framing a shot.
-camera_config = picam2.create_still_configuration(main={"size": (1920 , 1080)}, lores={"size": (320 , 180)}, display="lores")
+camera_config = picam2.create_still_configuration(main={"size": (80 , 45)}, lores={"size": (80 , 45)}, display="lores")
 #Load the configuration.
 picam2.configure(camera_config)
 #Start the preview window and then start the camera.
@@ -14,11 +16,17 @@ picam2.start()
 # pause for 2 seconds
 time.sleep(2)
 
-im = picam2.capture_array()
+rows = []
+for i in range(5):
+    im = picam2.capture_array()
+    greyed_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    rows.append({'i': i, 'img':greyed_image  })
+    
+output = pd.DataFrame(rows)
+output.to_csv('output.csv', index= False)
 
-im.dtype
 
-#picam2.stop()
-#picam2.stop_preview()
+picam2.stop()
+picam2.stop_preview()
 
 
